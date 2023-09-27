f40ee0d29ba993ad894c126dfa8c06c09cbab37b
emnordi <emnordi@gmail.com>
2023-09-27 22:24:52 +0200

    Attempt change map on swipe

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,5 +1,5 @@
 import Carousel from "react-spring-3d-carousel";
-import { useEffect, useState } from "react";
+import { SetStateAction, TouchEventHandler, useEffect, useState } from "react";
 import { config } from "@react-spring/web";
 import { CircuitFE } from "../../types/CircruitFE";
 import Cards from "./Cards";
@@ -39,8 +39,35 @@ const MapCarousel = ({
     setCards(table);
   }, [allCircuits]);
 
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e: any) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const onTouchMove = (e: any) => setTouchEnd(e.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isLeftSwipe || isRightSwipe)
+      setGoToSlide(isLeftSwipe ? goToSlide + 1 : goToSlide - 1);
+  };
+
   return (
-    <div style={{ width: width, height: height, margin: margin }}>
+    <div
+      style={{ width: width, height: height, margin: margin }}
+      onTouchEnd={onTouchEnd}
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+    >
       {cards?.length > 0 && (
         <Carousel
           slides={cards}
